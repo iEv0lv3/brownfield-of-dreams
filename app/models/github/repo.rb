2d033eaa4e25f4cc7a61a77f6a0386341d7fb909
Jordan <jordan.wallace.williams@gmail.com>
2020-03-26 13:01:24 -0600

    Update repo model to include id

--- app/models/github/repo.rb
+++ app/models/github/repo.rb
@@ -1,8 +1,9 @@
 class Repo
-  attr_reader :name, :url
+  attr_reader :name, :url, :id
 
   def initialize(data = Hash.new)
     @name = data[:name]
     @url = data[:html_url]
+    @id = data[:id]
   end
 end
