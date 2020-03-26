b6df0d3a9bdbad470d601edaf2f23f97843c70f4
Jordan <jordan.wallace.williams@gmail.com>
2020-03-26 11:05:04 -0600

    Update spec to include 2nd test with a different personal access token

--- spec/features/user/user_can_see_github_repos_spec.rb
+++ spec/features/user/user_can_see_github_repos_spec.rb
@@ -1,13 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe 'As a user', type: :feature do
-  before :each do
-    @user = create(:user)
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
-  end
 
-  describe 'When I view my dashboard', :vcr do
-    it 'I can see 5 of my GitHub repos listed' do
+  describe 'When I view my dashboard' do
+    vcr_options = { :record => :new_episodes }
+    it 'I can see 5 of my GitHub repos listed', :js, :vcr => vcr_options do
+
+      @user = create(:user, token: Figaro.env.github_personal_token)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+
       visit '/dashboard'
 
       within(".github") do
@@ -16,6 +17,25 @@ RSpec.describe 'As a user', type: :feature do
         expect(page).to have_link('algorithm_sort')
         expect(page).to have_link('b2-mid-mod')
         expect(page).to have_link('backend_module_0_capstone')
+      end
+    end
+  end
+
+  describe 'When I view my dashboard as a different user' do
+    vcr_options = { :record => :new_episodes }
+    it 'I can see 5 of my Github repos', :js, :vcr => vcr_options do
+
+      @user2 = create(:user, token: Figaro.env.github_personal_token2)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user2)
+
+      visit '/dashboard'
+
+      within(".github") do
+        expect(page).to have_link('monster_shop_1911')
+        expect(page).to have_link('battleship')
+        expect(page).to have_link('git_for_pairs_practice')
+        expect(page).to have_link('adopt_dont_shop')
+        expect(page).to have_link('b2-mid-mod')
       end
     end
   end
