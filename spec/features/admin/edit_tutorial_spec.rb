db2168258d6f1cb5a89a87cde541d7f837fe0795
Jordan <jordan.wallace.williams@gmail.com>
2020-03-26 11:05:47 -0600

    Update syntax

--- spec/features/admin/edit_tutorial_spec.rb
+++ spec/features/admin/edit_tutorial_spec.rb
@@ -4,14 +4,13 @@ describe "An Admin can edit a tutorial" do
   let(:tutorial) { create(:tutorial) }
   let(:admin)    { create(:admin) }
 
- vcr_options = {record: :new_episodes }
+  vcr_options = {record: :new_episodes }
   it "by adding a video", :js, :vcr => vcr_options do
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
 
     visit edit_admin_tutorial_path(tutorial)
 
     click_on "Add Video"
-    
 
     within "#new-video-form" do
       fill_in 'video_title', with: "How to tie your shoes."
