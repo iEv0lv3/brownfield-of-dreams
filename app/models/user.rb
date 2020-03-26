6b8296502f04ffba2ad0a80a1f39c71be22775d2
Jordan <jordan.wallace.williams@gmail.com>
2020-03-26 11:07:43 -0600

    Add my_repos model method that returns the user github repos

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
+require './app/models/github/repo'
+
 class User < ApplicationRecord
   has_many :user_videos
   has_many :videos, through: :user_videos
@@ -7,4 +9,12 @@ class User < ApplicationRecord
   validates_presence_of :first_name
   enum role: [:default, :admin]
   has_secure_password
+
+  def my_repos
+    github_service = GithubService.new(self.token)
+    repos = github_service.repos
+    repos.map do |repo|
+      Repo.new(repo)
+    end
+  end
 end
