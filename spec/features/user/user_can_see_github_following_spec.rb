87e08e621a569d3c413e5174d14417c44caae9b5
Jordan <jordan.wallace.williams@gmail.com>
2020-03-26 12:54:50 -0600

    Add spec to test that a user can see all github profiles they are following

---
+++ spec/features/user/user_can_see_github_following_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe 'As a user', type: :feature do
+
+  describe 'When I view my dashboard' do
+    vcr_options = { :record => :new_episodes }
+    it "I can see all the github profiles I'm following", :js, :vcr => vcr_options do
+
+      @user = create(:user, token: Figaro.env.github_personal_token)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+
+      visit '/dashboard'
+
+      within(".github") do
+        expect(page).to have_link('meredithkfwilliams')
+        expect(page).to have_link('alerrian')
+        expect(page).to have_link('adumortier')
+        expect(page).to have_link('d-atkins')
+        expect(page).to have_link('rcallen89')
+      end
+    end
+  end
+
+  describe 'When I view my dashboard as a different user' do
+    vcr_options = { :record => :new_episodes }
+    it "I can see all the github profiles I'm following", :js, :vcr => vcr_options do
+
+      @user2 = create(:user, token: Figaro.env.github_personal_token2)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user2)
+
+      visit '/dashboard'
+
+      within(".github") do
+        expect(page).to have_content('User is not following anyone.')
+      end
+    end
+  end
+end
