65af0425e19e1a590ebd0d73d60d6de72e0d7cbd
Jordan <jordan.wallace.williams@gmail.com>
2020-03-26 19:06:35 -0600

    Update omniauth callback route

--- config/routes.rb
+++ config/routes.rb
@@ -32,6 +32,9 @@ Rails.application.routes.draw do
   get '/about', to: 'about#show'
   get '/get_started', to: 'get_started#show'
 
+  # GitHub OmniAuth
+  get '/auth/:provider/callback', to: 'sessions#update'
+
   # Is this being used?
   get '/video', to: 'video#show'
 
