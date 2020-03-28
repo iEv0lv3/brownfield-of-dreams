87bdf075adb47715cb0d2b42810f991136254bf1
Pierce Alworth <pbalworth@gmail.com>
2020-03-28 18:05:55 -0500

    Add spec for creating tutorial admin

--- spec/features/admin/admin_can_make_new_tutorial_spec.rb
+++ spec/features/admin/admin_can_make_new_tutorial_spec.rb
@@ -7,22 +7,23 @@ RSpec.describe 'As an Admin' do
      end 
     it 'Can make a new tutorial' do 
         visit new_admin_tutorial_path
-        save_and_open_page
-        fill_in 'tutorial_title', with: "A New Tutorial"
-        fill_in 'tutorial_description', with: "A New description"
-        fill_in 'tutorial_thumbnail', with: "https://i.ytimg.com/vi/x3dNNlRxo8U/maxresdefault.jpg"
+        fill_in "tutorial[title]", with: "A New Tutorial"
+        fill_in "tutorial[description]", with: "A New description"
+        fill_in "tutorial[thumbnail]", with: "https://i.ytimg.com/vi/x3dNNlRxo8U/maxresdefault.jpg"
+        fill_in "video[video_id]", with: "https://i.ytimg.com/vi/x3dNNlRxo8U/maxresdefault.jpg"
+        fill_in "video[title]", with: "New Video"
+        fill_in "video[description]", with: "New description"
         click_on "Save"
-
         expect(current_path).to eq("/tutorials/#{Tutorial.last.id}")
         expect(page).to have_content("A New Tutorial")
         expect(page).to have_content("Successfully created tutorial.")
     end 
-#     it 'will see an error if not all info is filled in' do
-#         visit new_admin_tutorial_path
-#         fill_in 'tutorial_title', with: "new Tutorial"
-#         fill_in 'tutorial_thumbnail', with: "http://cdn3-www.dogtime.com/assets/uploads/2011/03/puppy-development-460x306.jpg"
-#         click_on "Save"
+    it 'will see an error if not all info is filled in' do
+        visit new_admin_tutorial_path
+        fill_in 'tutorial_title', with: "new Tutorial"
+        fill_in 'tutorial_thumbnail', with: "http://cdn3-www.dogtime.com/assets/uploads/2011/03/puppy-development-460x306.jpg"
+        click_on "Save"
 
-#         expect(page).to have_content("Please fill in all fields. Description can't be blank")
-#   end
+        expect(page).to have_content("Please fill in all fields. Description can't be blank")
+  end
 end 
