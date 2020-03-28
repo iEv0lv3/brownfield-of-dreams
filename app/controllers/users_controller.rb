92169cc0220a3b8d4187c574fe404c958fb4aeae
Jordan <jordan.wallace.williams@gmail.com>
2020-03-27 22:49:29 -0600

    Update users_controller create action to include account_activation mailer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,8 @@ class UsersController < ApplicationController
     user = User.create(user_params)
     if user.save
       session[:user_id] = user.id
+      AccountActivationMailer.inform(current_user).deliver_now
+      flash[:success] = 'Please check your email to activate account.'
       redirect_to dashboard_path
     else
       flash[:error] = 'Username already exists'
