f11991078294333033dbb848080d24f81cbc3b8c
Jordan <jordan.wallace.williams@gmail.com>
2020-03-29 16:00:28 -0600

    Rubocop syntax update

--- app/controllers/tutorials_controller.rb
+++ app/controllers/tutorials_controller.rb
@@ -1,8 +1,6 @@
 class TutorialsController < ApplicationController
   def show
-     if current_user == nil
-      flash[:notice] = "User must login to bookmark videos"
-    end
+    flash[:notice] = 'User must login to bookmark videos' if current_user.nil?
     tutorial = Tutorial.find(params[:id])
     @facade = TutorialFacade.new(tutorial, params[:video_id])
   end
