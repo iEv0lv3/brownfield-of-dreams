e9fcdf125284d67c73ca2114910f8529958a80eb
Jordan <jordan.wallace.williams@gmail.com>
2020-03-29 10:58:52 -0600

    Rubocop formatting

--- spec/features/admin/admin_can_delete_tutorial_spec.rb
+++ spec/features/admin/admin_can_delete_tutorial_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
-feature "An admin can delete a tutorial" do
-  scenario "and it should no longer exist" do
+feature 'An admin can delete a tutorial' do
+  scenario 'and it should no longer exist' do
     admin = create(:admin)
     create_list(:tutorial, 2)
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
 
-    visit "/admin/dashboard"
+    visit '/admin/dashboard'
 
     expect(page).to have_css('.admin-tutorial-card', count: 2)
 
@@ -17,7 +17,7 @@ feature "An admin can delete a tutorial" do
 
     expect(page).to have_css('.admin-tutorial-card', count: 1)
   end
-    it 'all videos are deleted when admin deletes video ' do
+  it 'all videos are deleted when admin deletes video ' do
     admin = create(:admin)
     tutorial1 = create(:tutorial)
     tutorial2 = create(:tutorial)
@@ -29,17 +29,16 @@ feature "An admin can delete a tutorial" do
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
 
-    visit "/admin/dashboard"
+    visit '/admin/dashboard'
 
     expect(page).to have_css('.admin-tutorial-card', count: 2)
 
     within(first('.admin-tutorial-card')) do
-      expect {
-      click_link'Delete'
-    }.to change { Video.count }.by(-2)
+      expect do
+        click_link 'Delete'
+      end.to change { Video.count }.by(-2)
     end
 
     expect(page).to have_css('.admin-tutorial-card', count: 1)
   end
-
 end
