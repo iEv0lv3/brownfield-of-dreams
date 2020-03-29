1cd882474fdd964f62b56715c1a1b00a9ac603b6
Jordan <jordan.wallace.williams@gmail.com>
2020-03-28 21:09:38 -0600

    Update all occurances of token with github_token, and add activation_token private method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 require './app/models/github/repo'
 require './app/models/github/follower'
+require 'securerandom'
 
 class User < ApplicationRecord
+  before_create :activation_token
+
   has_many :user_videos
   has_many :videos, through: :user_videos
 
@@ -9,10 +12,11 @@ class User < ApplicationRecord
   validates_presence_of :password
   validates_presence_of :first_name
   enum role: [:default, :admin]
+  enum activated: ['false', 'true']
   has_secure_password
 
   def my_repos
-    github_service = GithubService.new(self.token)
+    github_service = GithubService.new(self.github_token)
     repos = github_service.repos
     repos.map do |repo|
       Repo.new(repo)
@@ -20,7 +24,7 @@ class User < ApplicationRecord
   end
 
   def my_followers
-    github_service = GithubService.new(self.token)
+    github_service = GithubService.new(self.github_token)
     followers = github_service.followers
     followers.map do |follower|
       Follower.new(follower)
@@ -28,10 +32,18 @@ class User < ApplicationRecord
   end
 
   def following
-    github_service = GithubService.new(self.token)
+    github_service = GithubService.new(self.github_token)
     following = github_service.following
     following.map do |follower|
       Follower.new(follower)
+    end
+  end
+
+  private
+
+  def activation_token
+    if confirm_token.empty?
+      self.confirm_token = SecureRandom.hex(10)
     end
   end
 end
