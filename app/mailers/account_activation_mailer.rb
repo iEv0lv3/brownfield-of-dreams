4b0c2bf835ddc6832b3788919ce3c92d55c8f393
Jordan <jordan.wallace.williams@gmail.com>
2020-03-28 21:09:54 -0600

    Add account activation mailer

--- app/mailers/account_activation_mailer.rb
+++ app/mailers/account_activation_mailer.rb
@@ -1,6 +1,6 @@
 class AccountActivationMailer < ApplicationMailer
-  def inform(current_user)
-    @user = current_user
+  def inform(new_user)
+    @user = new_user
     mail(to: @user.email, subject: "#{@user.first_name}: Please activate your account!")
   end
 end
