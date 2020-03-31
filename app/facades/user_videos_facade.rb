77980bf23cee760f5f16686da104962fce33a866
Jordan <jordan.wallace.williams@gmail.com>
2020-03-30 21:29:48 -0600

    Refactor user_videos_facade to have the find_bookmarks and find_friends methods initialize

--- app/facades/user_videos_facade.rb
+++ app/facades/user_videos_facade.rb
@@ -1,9 +1,10 @@
 class UserVideosFacade
-  attr_reader :user, :bookmarks
+  attr_reader :user, :bookmarks, :user_friends
 
   def initialize(user)
     @user = User.find_by(id: user.id)
-    @bookmarks = nil
+    @bookmarks = find_bookmarks
+    @user_friends = find_friends
   end
 
   def find_bookmarks
@@ -11,5 +12,9 @@ class UserVideosFacade
       bm[video.tutorial_id] += [] << video
       bm
     end
+  end
+
+  def find_friends
+    @user.friends
   end
 end
