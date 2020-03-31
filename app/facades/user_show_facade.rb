5a9ce9229cad748e0248a7a1fbf45d5cff9f8f1d
Jordan <jordan.wallace.williams@gmail.com>
2020-03-31 10:10:27 -0600

    Update UserVideosFacade to UserShowFacade

---
+++ app/facades/user_show_facade.rb
@@ -0,0 +1,20 @@
+class UserShowFacade
+  attr_reader :user, :bookmarks, :user_friends
+
+  def initialize(user)
+    @user = User.find_by(id: user.id)
+    @bookmarks = find_bookmarks
+    @user_friends = find_friends
+  end
+
+  def find_bookmarks
+    @bookmarks = @user.my_bookmarks.reduce(Hash.new([])) do |bm, video|
+      bm[video.tutorial_id] += [] << video
+      bm
+    end
+  end
+
+  def find_friends
+    @user.friends
+  end
+end
